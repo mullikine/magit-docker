9781be4fef599629ecde54ed024e700d9882007f
Shane Mulligan <mullikine@gmail.com>
2020-09-25 15:07:04 +1200

    Fri 25 Sep 2020 15:07:04 NZST

--- run.sh
+++ run.sh
@@ -10,5 +10,7 @@ docker \
     "/$(pwd | slugify)" \
     -ti \
     --entrypoint= \
-    vlandeiro/magit:latest \
+    vlandeiro-magit-docker-030d5134:1.0 \
     /bin/sh -c "emacs --no-window-system --eval '(progn (magit-status) (delete-other-windows))'"
+
+# vlandeiro/magit:latest \
