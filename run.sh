e4501f8f88189a45dbf36d8eca96b751003733f1
Shane Mulligan <mullikine@gmail.com>
2020-09-25 15:48:33 +1200

    25.09.20

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 export TTY
 
+cd "$(vc get-top-level)"
+
 docker \
     run \
     --rm \
@@ -12,8 +14,3 @@ docker \
     --entrypoint= \
     vlandeiro-magit-docker-030d5134:1.0 \
     /bin/sh -c "eval \`resize\` && emacs --no-window-system --eval '(progn (magit-status) (delete-other-windows))'"
-
-# vlandeiro/magit:latest \
-# /bin/sh -c "emacs --no-window-system --eval '(progn (magit-status) (delete-other-windows))'"
-# docker run --rm -v "$(pwd):/$(pwd | slugify)" -w "/$(pwd | slugify)" -ti --entrypoint= vlandeiro-magit-docker-030d5134:1.0 /bin/sh -c "eval `resize` && emacs --no-window-system --eval '(progn (magit-status) (delete-other-windows))'"
-# nvt docker exec -it rstudio_s 
