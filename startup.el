1d475cd95fa5a49f2b5e1918eb3c0dee73e53a01
Shane Mulligan <mullikine@gmail.com>
2020-09-25 16:56:31 +1200

    Fri 25 Sep 2020 16:56:31 NZST

--- startup.el
+++ startup.el
@@ -2,6 +2,8 @@
 (add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
 (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
 (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+
 (package-initialize)
 (package-refresh-contents)
 (package-install 'magit nil)
